c0e17dd1f86d8a834397d20ed612f8f7ae209a6b
Amnish <amnishsingh04@gmail.com>
2023-05-29 22:12:27 -0400

    Add helper to tooltip

--- src/app/services/loading/loading.service.ts
+++ src/app/services/loading/loading.service.ts
@@ -5,14 +5,31 @@ import { Injectable } from '@angular/core';
 })
 export class LoadingService {
     private _loading: boolean = false;
+    private loadingInterval: any;
+    waitTime = 0; // Seconds
 
     constructor() { }
 
     set loading(loading: boolean) {
         this._loading = loading;
+
+        if (loading && !this.loadingInterval) {
+            this.loadingInterval = setInterval(() => {
+                ++this.waitTime;
+            }, 1000 * 1);
+        }
+        else {
+            clearInterval(this.loadingInterval);
+            this.loadingInterval = null;
+            this.waitTime = 0;
+        }
     }
 
     get loading(): boolean {
         return this._loading;
+    }
+
+    get showTooltip() {
+        return this.waitTime > 2;
     }
 }
